20c8f9f46f0c4e02dc6dfefb015b698d5e8423fb
ThatRadCat <iwan@usn.nl>
2024-02-08 14:53:43 +0100

    Changes it so it plays sounds at the right moment

--- amalucia-alert.py
+++ amalucia-alert.py
@@ -2,37 +2,40 @@ import asyncio
 import websockets
 import json
 from playsound import playsound
-from datetime import datetime, timezone
 import time
 
 async def connect_ais_stream():
 
     async with websockets.connect("wss://stream.aisstream.io/v0/stream") as websocket:
         subscribe_message = {"APIKey": "95ad04e180b70ef1c1e4f3c1455cecab1ec7d2df",
-                             "BoundingBoxes": [[[52.06075503467797, 5.103581306188966], [52.078569137579606, 5.077127968094469]]], # "box" aisstream looks in to find ships
-           #                  "FiltersShipMMSI": ["244615725", "244670379"],  # Filter per ship(s) comment out if you want to view all ships
+                             "BoundingBoxes": [[[52.06124712782111, 5.098014078839175], [52.07097541416011, 5.086389440933547]]], # "box" aisstream looks in to find ships
+#                             "FiltersShipMMSI": ["244021929"],  # Filter per ships comment out if you want to view all ships
                              "FilterMessageTypes": ["PositionReport"]} # Message filters 
         
-        subscribe_message_json = json.dumps(subscribe_message)
-        await websocket.send(subscribe_message_json)
+        subscribe_message_json = json.dumps(subscribe_message)  # Dump json msg from subscribe message
+        await websocket.send(subscribe_message_json)    
 
-        async for message_json in websocket:
+        async for message_json in websocket:        # filter message
             message = json.loads(message_json)
             message_type = message["MessageType"]
 
             if message_type == "PositionReport":
-             # the message parameter contains a key of the message type which contains the message itself
                 ais_message = message['Message']['PositionReport']
-                heading = ais_message['TrueHeading']
+             #   heading = ais_message['TrueHeading']
                 course = ais_message['Cog']
 
-            if 0 <= course <= 180:
+            if 0 <= course <= 180: # checks if course of ship is eastward
                 print(f"{ais_message['Cog']} Koers rechts {ais_message['UserID']}")
+                playsound('slimshady.mp3')
+                time.sleep(10)
+                playsound('../../Downloads/jobs_done.mp3')
 
-            if 181 <= course <= 370:
+            if 181 <= course <= 370: # checks if course of ship is westward
                 print(f"{ais_message['Cog']} Koers links {ais_message['UserID']}")
-       
-#                print(f"[{datetime.now(timezone.utc)}] ShipID: {ais_message['UserID']} Heading: {ais_message['TrueHeading']} Course: {ais_message['Cog']}")            
+                playsound('slimshady.mp3')
+                time.sleep(10)
+                playsound('../../Downloads/jobs_done.mp3')
 
+                time.sleep(360)
 
 asyncio.run(asyncio.run(connect_ais_stream()))
